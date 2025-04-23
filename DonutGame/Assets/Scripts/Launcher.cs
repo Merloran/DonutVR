e88e9c6e8ed382ac0ffe94071eeaa56fa583f53d
Merloran <poncyleuszmaciej@gmail.com>
2025-04-23 11:48:43 +0200

    Added fruit launcher

---
+++ DonutGame/Assets/Scripts/Launcher.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+
+public class Launcher : MonoBehaviour
+{
+    [SerializeField] 
+    public GameObject Prefab;
+
+    [SerializeField] 
+    public List<GameObject> Spawners;
+
+    [SerializeField]
+    public float Strength = 10.0f;
+    void Start()
+    {
+        StartCoroutine("LaunchFruit");
+    }
+
+    void Update()
+    {
+        
+    }
+
+    private IEnumerator LaunchFruit()
+    {
+        for (;;)
+        {
+            var spawner = Spawners[Random.Range(0, Spawners.Count)];
+
+            var fruit = Instantiate(Prefab, spawner.transform);
+            fruit.GetComponent<Rigidbody>().AddForce((spawner.transform.forward + spawner.transform.up).normalized * Strength, ForceMode.Impulse);
+            yield return new WaitForSeconds(5.0f);
+            Destroy(fruit);
+        }
+    }
+}
